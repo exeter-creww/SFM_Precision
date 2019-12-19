d2cf775c05ca8422db835a2c460746c5e5e54ba6
h-a-graham <hugh.a.graham@gmail.com>
2019-12-19 20:18:27 +0000

    Added new module to process point clouds. Early days yet. see __init.py__ for notes...

--- PointCloudStat/__init__.py
+++ PointCloudStat/__init__.py
@@ -6,3 +6,11 @@ from PointCloudStat import DSM
 from PointCloudStat import precision_map
 from PointCloudStat import DEM_of_difference
 
+# I'm thinking this should have the following functions:
+#
+# generate elevation raster (as class object)
+# generate precision raster (as class object)
+# generate dem of difference (as class object)
+# save raster (for both DEm and prec rasters)
+# plot raster
+# plot histogram
