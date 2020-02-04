mask_AOI.py had a major bug which is now fixed. mask works correctly and has been added back into the difference module. Also reverted change on max precision value. This could still be improved but will do for now.

sfm_gridz/plot_gridz.py
@@ -25,7 +25,7 @@ def plot_raster(raster, band, cmap, save_path, dpi, v_range, title, obs):
             v_range = (np.nanmin(arr), np.nanmax(arr))
 
 
-        fig, ax = plt.subplots(figsize=(6, 8))
+        fig, ax = plt.subplots(figsize=(5, 8))
 
         img = ax.imshow(arr, vmin=v_range[0], vmax=v_range[1], cmap=cmap,
                         extent=(ras.bounds[0], ras.bounds[2], ras.bounds[1], ras.bounds[3]))

sfm_gridz/precision_map.py
@@ -58,7 +58,7 @@ class PrRas:
 
         self.min_res = math.ceil(min_val * 10000) / 10000
 
-        # self.max_prec = math.ceil(np.nanmax(pcdata['zerr'], 95) * 10000) / 10000
+        self.max_prec = math.ceil(np.nanmax(pcdata['zerr']) * 10000) / 10000
 
 
     def Run(self):
@@ -129,7 +129,7 @@ class PrRas:
             arr = src.read(1)
 
         arr_fill = np.copy(arr)
-        self.max_prec = np.nanmax(arr_fill)
+        # self.max_prec = np.nanmax(arr_fill)
         arr_fill[arr_fill == -999] = self.max_prec
         meta.update(count=2)
 